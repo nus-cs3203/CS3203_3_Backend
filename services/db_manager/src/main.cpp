c9efc02640ef30c5cc54d65f45cf4d900138e5c2
Albert Ariel Widiaatmaja <albertariel2002@gmail.com>
2025-02-04 08:56:40 +0800

    feat: general mongodb functions create_collection, insert_document, delete_document, find_document

--- services/db_manager/src/main.cpp
+++ services/db_manager/src/main.cpp
@@ -11,13 +11,59 @@
 using bsoncxx::builder::basic::kvp;
 using bsoncxx::builder::basic::make_document;
 
-int main() {
-    mongocxx::instance instance;
-    mongocxx::uri uri("<connection string>");
-    mongocxx::client client(uri);
-    auto db = client["sample_mflix"];
-    auto collection = db["movies"];
+class Database {
+public:
+    Database(const std::string& uri, const std::string& db_name)
+        : instance_{}, client_{mongocxx::uri{uri}}, db_{client_[db_name]} {}
 
-    auto result = collection.find_one(make_document(kvp("title", "The Shawshank Redemption")));
-    std::cout << bsoncxx::to_json(*result) << std::endl;
+    void create_collection(const std::string& collection_name) {
+        db_.create_collection(collection_name);
+        std::cout << "Collection '" << collection_name << "' created successfully." << std::endl;
+    }
+
+    void insert_document(const std::string& collection_name, const bsoncxx::document::view_or_value& doc) {
+        auto collection = db_[collection_name];
+        collection.insert_one(doc);
+        std::cout << "Document inserted into '" << collection_name << "'." << std::endl;
+    }
+
+    void delete_document(const std::string& collection_name, const bsoncxx::document::view_or_value& filter) {
+        auto collection = db_[collection_name];
+        auto result = collection.delete_one(filter);
+        if (result && result->deleted_count() > 0) {
+            std::cout << "Document deleted from '" << collection_name << "'." << std::endl;
+        } else {
+            std::cout << "No matching document found to delete." << std::endl;
+        }
+    }
+
+    void find_document(const std::string& collection_name, const bsoncxx::document::view_or_value& filter) {
+        auto collection = db_[collection_name];
+        auto result = collection.find_one(filter);
+        if (result) {
+            std::cout << "Found document: " << bsoncxx::to_json(*result) << std::endl;
+        } else {
+            std::cout << "No matching document found." << std::endl;
+        }
+    }
+
+private:
+    mongocxx::instance instance_;
+    mongocxx::client client_;
+    mongocxx::database db_;
+};
+
+int main() {
+    const std::string uri = "mongodb://127.0.0.1:27017";
+    Database db(uri, "sample_mflix");
+    
+    db.create_collection("movies");
+
+    db.insert_document("movies", make_document(kvp("title", "The Shawshank Redemption"), kvp("year", 1994)));
+    
+    db.find_document("movies", make_document(kvp("title", "The Shawshank Redemption")));
+    
+    db.delete_document("movies", make_document(kvp("title", "The Shawshank Redemption")));
+    
+    return 0;
 }
