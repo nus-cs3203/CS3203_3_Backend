535d03699460616c88938ec71be28a3b017cd5a4
Albert Ariel Widiaatmaja <albertariel2002@gmail.com>
2025-02-13 02:48:01 +0800

    chore: tidy up spacing in database wrapper class

--- common/include/database.hpp
+++ common/include/database.hpp
@@ -11,48 +11,62 @@
 
 class Database {
 public:
-    Database(const std::string& uri, const std::string& db_name);
+    Database(
+        const std::string& uri, 
+        const std::string& db_name
+    );
 
-    auto find_one(const std::string& collection_name,
-                  const bsoncxx::document::view& filter,
-                  const mongocxx::options::find& option = {})
-        -> bsoncxx::stdx::optional<bsoncxx::document::value>;
+    auto find_one(
+        const std::string& collection_name,
+        const bsoncxx::document::view& filter,
+        const mongocxx::options::find& option = {}
+    ) -> bsoncxx::stdx::optional<bsoncxx::document::value>;
 
-    auto find(const std::string& collection_name,
-            const bsoncxx::document::view& filter,
-            const mongocxx::options::find& option = {})
-      -> mongocxx::cursor;
+    auto find(
+        const std::string& collection_name,
+        const bsoncxx::document::view& filter,
+        const mongocxx::options::find& option = {}
+    ) -> mongocxx::cursor;
 
-    auto insert_one(const std::string& collection_name,
-                    const bsoncxx::document::view& document)
-        -> bsoncxx::stdx::optional<mongocxx::result::insert_one>;
+    auto insert_one(
+        const std::string& collection_name,
+        const bsoncxx::document::view& document
+    ) -> bsoncxx::stdx::optional<mongocxx::result::insert_one>;
 
-    auto insert_many(const std::string& collection_name,
-        const std::vector<bsoncxx::document::value>& documents)
-        -> bsoncxx::stdx::optional<mongocxx::result::insert_many>;
+    auto insert_many(
+        const std::string& collection_name,
+        const std::vector<bsoncxx::document::value>& documents
+    ) -> bsoncxx::stdx::optional<mongocxx::result::insert_many>;
 
-    auto delete_one(const std::string& collection_name,
-                    const bsoncxx::document::view& filter)
-        -> bsoncxx::stdx::optional<mongocxx::result::delete_result>;
+    auto delete_one(
+        const std::string& collection_name,
+        const bsoncxx::document::view& filter
+    ) -> bsoncxx::stdx::optional<mongocxx::result::delete_result>;
 
-        auto delete_many(const std::string& collection_name,
-            const bsoncxx::document::view& filter)
-        -> bsoncxx::stdx::optional<mongocxx::result::delete_result>;
+    auto delete_many(
+        const std::string& collection_name,
+        const bsoncxx::document::view& filter
+    ) -> bsoncxx::stdx::optional<mongocxx::result::delete_result>;
 
-    auto update_one(const std::string& collection_name,
-                    const bsoncxx::document::view& filter,
-                    const bsoncxx::document::view& update_document,
-                    const bool& upsert)
-        -> bsoncxx::stdx::optional<mongocxx::result::update>;
+    auto update_one(
+        const std::string& collection_name,
+        const bsoncxx::document::view& filter,
+        const bsoncxx::document::view& update_document,
+        const bool& upsert
+    ) -> bsoncxx::stdx::optional<mongocxx::result::update>;
 
 
-    auto update_many(const std::string& collection_name,
-                     const bsoncxx::document::view& filter,
-                     const bsoncxx::document::view& update_document,
-                     const bool& upsert)
-        -> bsoncxx::stdx::optional<mongocxx::result::update>;
+    auto update_many(
+        const std::string& collection_name,
+        const bsoncxx::document::view& filter,
+        const bsoncxx::document::view& update_document,
+        const bool& upsert
+    ) -> bsoncxx::stdx::optional<mongocxx::result::update>;
 
-    auto aggregate(const std::string& collection_name, const mongocxx::pipeline& pipeline) -> mongocxx::cursor;
+    auto aggregate(
+        const std::string& collection_name, 
+        const mongocxx::pipeline& pipeline
+    ) -> mongocxx::cursor;
 
 private:
     mongocxx::instance instance; 
