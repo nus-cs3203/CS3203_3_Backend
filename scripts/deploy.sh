fc26209295430f746734369629bc638d578eb7e0
Albert Ariel Widiaatmaja <albertariel2002@gmail.com>
2025-02-16 22:58:30 +0800

    feat: bulk uploader + deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,2 +1,2 @@
-docker buildx build --platform linux/amd64 -t albertariel/cs3203_3_backend-pages_admin_analytics:0.0.4 -f ./services/pages/admin/analytics/Dockerfile .
-docker push albertariel/cs3203_3_backend-pages_admin_analytics:0.0.4
+docker buildx build --platform linux/amd64 -t albertariel/cs3203_3_backend-pages_admin_analytics:0.0.5 -f ./services/pages/admin/analytics/Dockerfile .
+docker push albertariel/cs3203_3_backend-pages_admin_analytics:0.0.5
