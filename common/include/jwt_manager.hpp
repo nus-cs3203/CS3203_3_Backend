2e256380703d753bbe11f65d18472dc833fd8211
Albert Ariel Widiaatmaja <albertariel2002@gmail.com>
2025-03-08 20:34:39 +0800

    feat: jwt_manager

--- common/include/jwt_manager.hpp
+++ common/include/jwt_manager.hpp
@@ -2,12 +2,24 @@
 #define JWT_MANAGER_HPP
 
 #include "constants.hpp"
+#include "env_manager.hpp"
 
 #include <string>
 
 class JwtManager {
 public:
-    
+    JwtManager(
+        const std::string& jwt_secret = env_manager.read_env("JWT_SECRET", "")
+    );
+
+    std::string generate_token(const std::string &role);
+
+    std::string get_user_role_from_token(const std::string &token);
+
+private:
+    std::string jwt_secret;
+
+    static EnvManager env_manager;
 };
 
 #endif // JWT_MANAGER_HPP
---
+++ common/src/jwt_manager.cpp
@@ -0,0 +1,39 @@
+#include "jwt_manager.hpp"
+
+#include <jwt-cpp/jwt.h>
+
+#include <chrono>
+#include <stdexcept>
+
+std::string JwtManager::generate_token(const std::string &role) {
+    auto token = jwt::create()
+        .set_issuer("auth0")
+        .set_type("JWS")
+        .set_payload_claim("role", jwt::claim(role))
+        .set_expires_at(std::chrono::system_clock::now() + std::chrono::hours(1))
+        .sign(jwt::algorithm::hs256{jwt_secret});
+    
+    return token;
+}
+
+std::string JwtManager::get_user_role_from_token(const std::string &token) {
+    try {
+        auto decoded = jwt::decode(token);
+
+        auto verifier = jwt::verify()
+            .allow_algorithm(jwt::algorithm::hs256{jwt_secret})
+            .with_issuer("auth0");
+        verifier.verify(decoded);
+
+        auto roleClaim = decoded.get_payload_claim("role").as_string();
+        return roleClaim;
+    } catch (const std::exception &e) {
+        throw std::runtime_error(std::string("Token verification failed: ") + e.what());
+    }
+}
+
+EnvManager JwtManager::env_manager = EnvManager();
+
+JwtManager::JwtManager(
+    const std::string& jwt_secret
+) : jwt_secret{jwt_secret} {}
