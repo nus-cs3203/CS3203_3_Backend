bd4b626228b9c485b8c484aa45f226ccf9f2e841
Albert Ariel Widiaatmaja <albertariel2002@gmail.com>
2025-03-17 15:20:58 +0800

    feat: deployment script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,2 +1,5 @@
 docker buildx build --platform linux/amd64 -t albertariel/cs3203_3_backend-pages_admin_analytics:0.0.7 -f ./services/pages/admin/analytics/Dockerfile .
 docker push albertariel/cs3203_3_backend-pages_admin_analytics:0.0.7
+
+docker buildx build --platform linux/amd64 -t albertariel/cs3203_3_backend-management:0.0.2 -f ./services/management/Dockerfile .
+docker push albertariel/cs3203_3_backend-management:0.0.2
