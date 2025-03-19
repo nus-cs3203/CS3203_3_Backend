c03cd3f239c015e73ab4c5db1212b71e3280d5c1
Albert Ariel Widiaatmaja <albertariel2002@gmail.com>
2025-03-19 17:35:23 +0800

    feat: get_one

--- services/management/include/management_api_strategy.hpp
+++ services/management/include/management_api_strategy.hpp
@@ -13,7 +13,6 @@
 
 namespace ManagementApiStrategy {
     auto process_request_func_get_one_by_oid(const crow::request& req) -> std::tuple<bsoncxx::document::value, mongocxx::options::find>; 
-    auto process_response_func_get_one(const bsoncxx::document::value& doc) -> crow::json::wvalue;
 
     auto process_request_func_get_many(const crow::request& req) -> std::tuple<bsoncxx::document::value, mongocxx::options::find>;
 
--- services/management/src/management_api_strategy.cpp
+++ services/management/src/management_api_strategy.cpp
@@ -46,16 +46,6 @@ auto ManagementApiStrategy::process_request_func_get_many(const crow::request& r
     return std::make_tuple(filter, option);
 }
 
-
-auto ManagementApiStrategy::process_response_func_get_one(const bsoncxx::document::value& doc) -> crow::json::wvalue {
-    crow::json::wvalue response_data;
-    auto doc_json = bsoncxx::to_json(doc);
-    auto doc_rval = crow::json::load(doc_json);
-    auto pased_doc_wval = BaseApiStrategyUtils::parse_database_json_to_response_json(doc_rval);
-    response_data["document"] = std::move(pased_doc_wval);
-    return response_data;
-}
-
 auto ManagementApiStrategy::process_request_func_get_all(const crow::request& req) -> std::tuple<bsoncxx::document::value, mongocxx::options::find> {    
     auto filter = make_document();
     mongocxx::options::find option;
