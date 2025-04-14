b6ade71166b44e95d3a3111b7528324d7ca539a2
Albert Ariel Widiaatmaja <albertariel2002@gmail.com>
2025-04-15 00:43:37 +0800

    fix: test make issue in dockers

--- scripts/make.sh
+++ scripts/make.sh
@@ -2,4 +2,5 @@ cd ../build
 cmake .. \
     -DCMAKE_BUILD_TYPE=RelWithDebInfo \
     -DCMAKE_INSTALL_PREFIX=../release \
-    -DCROW_INCLUDE_DIR="../libs/Crow/include/"
+    -DCROW_INCLUDE_DIR="../libs/Crow/include/" \
+    -DENABLE_TESTING=ON
